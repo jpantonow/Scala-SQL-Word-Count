4e0283253ea5a8ad3ed3024bd02962eead975ba9
jp4ntsnow <jpantonow@gmail.com>
2023-09-27 00:33:16 -0300

    insert char

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -4,8 +4,8 @@ object Main extends App {
     //Execução das funções do banco de dados
     val db_create_tables = new CreateTables()
     val db_insert_words = new Insert_Words()
-    //val db_select_words = new Select_Words()
-
+    val db_select_words = new Select_Words()
+    val db_select_char = new Select_Characters()
     //Caso de sucesso
     val greenColor = "\u001B[32m"
     val resetColor = "\u001B[0m"
