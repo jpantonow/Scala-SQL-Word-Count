c81c9b03461670e655e3c0045cb1ff3e323c21b0
jp4ntsnow <jpantonow@gmail.com>
2023-10-02 18:34:03 -0300

    reducado da interaction

--- src/main/scala/components/Interaction.scala
+++ src/main/scala/components/Interaction.scala
@@ -1,9 +1,9 @@
 package components
 import java.io.File
 
+//Classe para interagir com o usuario
 class Interaction{
-    var (txt_file, book_name) = display_texts
-    var db_file: String = display_databases
+    var (txt_file, book_name, db_file) = display_texts
 
     def print_message(string: String): Unit = {
         val greenColor = "\u001B[32m"
@@ -11,25 +11,23 @@ class Interaction{
         println(greenColor + string + resetColor)
     }
     
-    def display_texts: (String, String) = {
+    def display_texts: (String, String, String) = {
+
         println("\nWelcome, user! Which File would you like to read?\n")
         val directory_path = new File("src/main/scala/texts")
         val contents = directory_path.list()
+
         for(i <- 0 until contents.size){
             println(i + "-" + contents(i))
         }
+
         var chosen = scala.io.StdIn.readInt()
         var book = contents(chosen).replace(".txt", "")
         var file = directory_path.getPath() + "/" + contents(chosen)
-        return (file, book)
-    }
+        val db_path = new File("src/main/scala/sqlite3/databases")
+        var database = db_path.getPath() + "/" + book + ".db"
 
-    def display_databases: String = {
-        //println("There is already a database named this way.")
-        //println("\nWhich Database would you like to write?\n")
-        val directory_path = new File("src/main/scala/sqlite3/databases")
-        var database = directory_path.getPath() + "/" + book_name + ".db"
-        return database
+        return (file, book, database)
     }
 
     def export_message: String = {
