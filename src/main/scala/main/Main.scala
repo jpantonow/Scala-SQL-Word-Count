184e2c100bae9463a6efbde3f232aed7b417be29
jp4ntsnow <jpantonow@gmail.com>
2023-10-04 10:26:41 -0300

    longest_word ta dando erro o resto consertei

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -3,10 +3,5 @@ import components._
 import java.io.File
 object Main extends App {
     val WordCount = new WordCount()
-    WordCount.create
-    WordCount.register_doc
-    WordCount.contar
-    WordCount.register_updates
-    WordCount.selecionar
-    WordCount.export_csv
+    WordCount.execute
 }
