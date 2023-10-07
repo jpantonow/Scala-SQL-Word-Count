d9dcf0a958de31ca25b1b58a5903d7ced133aa88
jp4ntsnow <jpantonow@gmail.com>
2023-10-07 14:32:31 -0300

    test.txt in the right folder

--- src/test/scala/components/db/RegisterDocumentsTest.scala
+++ src/test/scala/components/db/RegisterDocumentsTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class RegisterDocumentsTest extends AnyFunSuite {
   test("The count_words method should return true") {
     val db_register = new Register_Documents(
-      "src/main/scala/files/books/test.txt",
+      "src/test/scala/files/books/test.txt",
       "src/test/scala/components/db/databasetest.db",
       "test.txt"
     )
