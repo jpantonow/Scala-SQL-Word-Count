7f376cb3fbd95c798b87ee8650e725a5e1ed669e
jp4ntsnow <jpantonow@gmail.com>
2023-10-07 15:04:34 -0300

    test.txt in the right folder

--- src/test/scala/components/db/DbCommandsTest.scala
+++ src/test/scala/components/db/DbCommandsTest.scala
@@ -1,4 +1,13 @@
 package components.db
 import org.scalatest.funsuite.AnyFunSuite
 
-class DbCommandsTest extends AnyFunSuite {}
+class DbCommandsTest extends AnyFunSuite {
+    test("The execute method should return true") {
+        val db_create = new CreateTables(
+        "src/test/scala/files/books/test.txt",
+        "src/test/scala/components/db/databasetest.db",
+        "test.txt"
+        )
+  }
+
+}
--- src/test/scala/components/db/ExportCSVTest.scala
+++ src/test/scala/components/db/ExportCSVTest.scala
@@ -1,4 +1,13 @@
 package components.db
 import org.scalatest.funsuite.AnyFunSuite
 
-class ExportCSVTest extends AnyFunSuite {}
+class ExportCSVTest extends AnyFunSuite {
+    test("The export_words method should return true"){
+        val db_export = new Export_to_CSV(
+            "src/test/scala/files/books/test.txt",
+            "src/test/scala/components/db/databasetest.db",
+            "test.txt"
+        )
+        assert(db_export.export_words == true)
+    }
+}
