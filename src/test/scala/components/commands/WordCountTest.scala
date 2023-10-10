0f82ca512312e9194cefab94749df6c666a80bee
jp4ntsnow <jpantonow@gmail.com>
2023-10-10 13:20:46 -0300

    doodoo

--- src/test/scala/components/commands/WordCountTest.scala
+++ src/test/scala/components/commands/WordCountTest.scala
@@ -2,7 +2,7 @@ package components.commands
 import org.scalatest.funsuite.AnyFunSuite
 
 class WordCountTest extends AnyFunSuite {
-  test("The most frequent world should be \"tubarão\""){
+  test("The most frequent world should be \"doodoo\""){
     val wordCount = new WordCount()
     wordCount.set_pathing(
       "src/test/scala/files/books/teste.txt",
@@ -13,9 +13,9 @@ class WordCountTest extends AnyFunSuite {
     wordCount.set_limit(5)
     wordCount.run
     assert(wordCount.check_existence == true)
-    // var frequencias = wordCount.get_frequency
-    // var listaPalavras = frequencias.words
-    // assert(listaPalavras(0) == ("tubarão", 20))
+    var frequencias = wordCount.get_frequency
+    var listaPalavras = frequencias.words
+    assert(listaPalavras(0) == ("doodoo",81))
   }
   // test("The most frequent word should be \"tubarão\"") {
   //    val wordCount = new WordCount(
